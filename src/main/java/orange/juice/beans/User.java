c55b62ea04ca32470f11d80348c1321621aac94d
brian <brian.conlon18@myhunter.cuny.edu>
2021-11-07 11:37:17 -0600

    lots of commit

---
+++ src/main/java/orange/juice/beans/User.java
@@ -0,0 +1,5 @@
+package orange.juice.beans;
+
+public class User {
+
+}
---
+++ src/test/java/orange/juice/ApplicationTests.java
@@ -0,0 +1,13 @@
+package orange.juice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
